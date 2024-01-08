7240fdf71ef9fbde9c7e51c6e21223fca3a283b7
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-08 12:29:40 +0530

    second commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Singin from "./pages/Singin"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+
 
 export default function App() {
-  return (
-    <h1 className='text-center , text-red-600' >App</h1>
+  return <BrowserRouter>
+
+  <Routes>
+
+    <Route path="/" element={<Home/>}/>  
+    <Route path="/Sign-up" element={<SignUp/>}/> 
+    <Route path="/Sign-in" element={<Singin/>}/> 
+    <Route path="/about" element={<About/>}/> 
+    <Route path="/profile" element={<Profile/>}/> 
+
+  </Routes>
+
+  
+  </BrowserRouter>
     
-  )
+    
+  
 }
 
 
