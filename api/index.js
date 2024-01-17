74a55a155a00898bc5ec87a15333fd0163927453
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-17 18:35:04 +0530

    auth created

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
+import authRoute from "./routes/auth.route.js"
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,6 +11,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err)
 })
 const app = express()
+app.use(express.json())
 
 app.listen(3000 ,() =>{
 
@@ -19,3 +21,4 @@ app.listen(3000 ,() =>{
 
 
 app.use("/api/user",userRoute);
+app.use("/api/auth",authRoute);
