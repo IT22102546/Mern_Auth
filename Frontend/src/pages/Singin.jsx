3dfe76e17b20132917d77e2f08845e87c9637f64
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-20 21:58:35 +0530

    redux done

--- Frontend/src/pages/Singin.jsx
+++ Frontend/src/pages/Singin.jsx
@@ -1,20 +1,21 @@
 
 import { useState } from 'react';
 import {Link , useNavigate} from 'react-router-dom';
+import { signInStart,signInSuccess,singInFailure } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SignIn() {
   const [formData , setFormData] = useState({});
-  const[error , setError] = useState(false);
-  const[loading , setLoading] = useState(false);
+  const{loading , error} = useSelector((state)=>state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) =>{
     setFormData({...formData, [e.target.id]:e.target.value});
   } 
   const handleSubmit = async (e)=>{
     e.preventDefault();
     try{
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -24,17 +25,17 @@ export default function SignIn() {
       });
   
       const data = await res.json();
-      setLoading(false);
+      
      if(data.success == false){
-      setError(true);
+      dispatch(singInFailure(data));
       return;
      }
+     dispatch(signInSuccess(data));
 
       navigate('/');
 
     }catch(error){
-      setLoading(false);
-      setError(true)
+      dispatch(singInFailure(error));
     }
    
     
@@ -42,7 +43,7 @@ export default function SignIn() {
  
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input type="text" placeholder="Email" id="email" className="bg-slate-200  p-3 rounded-lg" onChange={handleChange}/>
         <input type="text" placeholder="Password" id="password" className="bg-slate-200  p-3 rounded-lg" onChange={handleChange}/>
@@ -55,7 +56,7 @@ export default function SignIn() {
         </Link>
        
       </div>
-      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+      <p className='text-red-700 mt-5'>{error ? error.message || 'Something went wrong!' : ""}</p>
     </div>
   )
 }
