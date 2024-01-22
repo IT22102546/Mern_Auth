a4ac1f7c906cb6d931cc9e83d0c7ad498d4c6ece
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-22 22:52:41 +0530

    Updating route created

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
 import authRoute from "./routes/auth.route.js"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -10,8 +11,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err)
 })
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000 ,() =>{
 
