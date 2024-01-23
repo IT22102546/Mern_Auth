13e54540115177d446eeb4a771f3e33c8f54c6f7
Sanjana Nimesh <127377258+IT22102546@users.noreply.github.com>
2024-01-24 00:34:43 +0530

    deleting function finished

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux" ;
 import { useRef, useState , useEffect } from "react";
 import  { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart , updateUserFailure , updateUserSuccess } from "../redux/user/userSlice";
+import { updateUserStart , updateUserFailure , updateUserSuccess, deleteUserStart, deleteUserSuccess , deleteUserFailure } from "../redux/user/userSlice";
 
 export default function Profile() {
   const dispatch = useDispatch();
@@ -77,6 +77,23 @@ export default function Profile() {
 
   }
 
+  const handleDeleteAccount = async ()=>{
+    try {
+      dispatch (deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method : "DELETE"
+      });
+      const data  = await res.json();
+      if(data.success === false){
+        dispatch(deleteUserFailure());
+        return;
+      }
+      dispatch(deleteUserSuccess());
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -98,7 +115,7 @@ export default function Profile() {
         hover:opacity-95 disabled:opacity-80">{loading ? 'Loading...' : 'Update'}</button>
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span onClick={handleDeleteAccount} className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error && "something went wrong"}</p>
